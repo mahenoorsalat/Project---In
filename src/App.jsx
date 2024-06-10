19a66da4012cea4868154ba07112ed33d4672e01
Mahenoor <salatmahenoor7.8.6@gmail.com>
2024-06-10 22:35:36 +0530

    New File addded

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,17 @@ function App() {
 <>
 
 <Navbar/>
-
-
+<div className="container">
+  
+<div className='w-[500px] h-[500px] bg-red-400 flex justify-center rounded-2xl'>
+   <div className='w-80 h-80 bg-red-500 justify-end m-24 float-end space-x-56 shadow-2xl rounded-2xl'>
+    <img src="Profile picture.png" alt="Add any image here bcause error is showing after I can correct it." />
+   </div>
+  </div>
+ 
+  
+  
+</div>
 </>
 
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,11 @@ const Navbar = () => {
       </nav>
 
      
-    </div></>
+    </div>
+    
+    
+      
+      </>
   )
 }
 
