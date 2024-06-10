1c9518c37419ffe6eaab8d71ca87478908927f4d
hafizasad419 <hafizasad419@gmail.com>
2024-06-10 12:46:08 +0500

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+
+<>
+
+<h1 className="main-heading py-4 bg-red-300 text-white text-xl font-bold">
+  Hey Mahenoor, Welcome to the project
+</h1>
+
+</>
+
+  )
+}
+
+export default App
