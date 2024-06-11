cc450244f764fc42b0a8451e61e40002ad6cc587
hafizasad419 <hafizasad419@gmail.com>
2024-06-11 11:31:48 +0500

    fix img loading error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar'
-
+import asadImg from './assets/asad-candid.jpg'
+//add your img in src/assets folder and just update the import statement and src attribute here. 
 
 function App() {
 
@@ -12,8 +13,10 @@ function App() {
 <div className="container">
   
 <div className='w-[500px] h-[500px] bg-red-400 flex justify-center rounded-2xl'>
-   <div className='w-80 h-80 bg-red-500 justify-end m-24 float-end space-x-56 shadow-2xl rounded-2xl'>
-    <img src="Profile picture.png" alt="Add any image here bcause error is showing after I can correct it source." />
+   <div className='flex justify-center w-80 h-80 bg-red-500 m-24 space-x-56 shadow-2xl rounded-full'>
+    <img
+    className='w-full h-full rounded-full '
+    src={asadImg} alt="Done" />
    </div>
   </div>
  
